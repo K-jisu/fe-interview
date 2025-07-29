9c52efbb2bbb519d79eb2eb27ab744a2ffdd6959
K-jisu <wltn7star@naver.com>
2025-07-29 18:51:59 +0900

    style: 레이아웃에서 구글 폰트 제거 및 네비게이션 바 컴포넌트 추가 #11

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "../widgets/layout/navbar";
 
 export const metadata: Metadata = {
   title: "FronterView",
@@ -24,10 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <Navbar />
+        <main className="min-h-screen bg-gray-50">{children}</main>
       </body>
     </html>
   );
