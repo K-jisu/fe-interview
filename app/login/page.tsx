1a850457c588d3bc8fe767c17e0f4a52f635b520
K-jisu <wltn7star@naver.com>
2025-07-31 02:39:15 +0900

    style: 로그인 페이지 UI 구성 요소 추가 - 로그인 폼 및 헤더 컴포넌트 구현#8

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,15 @@
+import LoginForm from "../../widgets/login/ui/login-form";
+import LoginHeader from "../../widgets/login/ui/login-header";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className="container mx-auto px-4 py-8 md:py-16">
+      <div className="max-w-md mx-auto">
+        <LoginHeader />
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
